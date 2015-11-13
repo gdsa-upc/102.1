0e60b498f10156585c0bda729fae88589b1e802c
victor_prt <yuni_1991@hotmail.com>
2015-11-13 17:08:02 +0100

    revisio primer script fet amb opencv i extraccio surf

--- projecte.py
+++ projecte.py
@@ -1,21 +1,10 @@
-from skimage import data
-from skimage.feature import CENSURE
-from skimage.color import rgb2gray
+import numpy as np
+import cv2
 import matplotlib.pyplot as plt
 
-
-img1 = rgb2gray(data.astronaut())
-
-detector = CENSURE()
-
-plt.gray()
-
-detector.detect(img1)
-
-plt.imshow(img1)
-plt.axis('off')
-plt.scatter(detector.keypoints[:, 1], detector.keypoints[:, 0],
-              2 ** detector.scales, facecolors='none', edgecolors='r')
-
-
-plt.show()
+img = cv2.imread('/home/victor/Escritorio/uni/tercer curso/Q5/GDSA/102.1/images/people.jpg',0)
+surf = cv2.SURF(4000)
+kp, des = surf.detectAndCompute(img, None)
+img2 = cv2.drawKeypoints(img, kp, None, (255,0,0), 4)
+plt.imshow(img2),plt.show()
+print len(kp)
