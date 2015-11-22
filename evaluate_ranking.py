f2a5dde3d1da6191657378083508cc7d877bf4d4
Eric_DC <heimricusdc@gmail.com>
2015-11-23 00:24:46 +0100

    Retoques evaluate_ranking.py
    keep on working dude, just keep on working

--- evaluate_ranking.py
+++ evaluate_ranking.py
@@ -6,11 +6,12 @@ import os # Carreguem la llibreria corresponent a la funció de introducció de
 from sklearn.metrics import label_ranking_average_precision_score
 
 ruta_abs = os.path.dirname(os.path.abspath(__file__)) #Obtenim la ruta absoluta de la carpeta on es troben els fitxers
-#Direct_llista = rank(ruta_abs+'/files',ruta_abs+'/files',ruta+'/files/features_train.p',"valid") #Invoquem a la funció rank.py per obtenir el directori de la llista classificada d'imatge
+Direct_llista_t = open(ruta_abs+'\\files\\features_train.p','r') #Llegim els fitxer de la llista per entrenament
+Direct_llista_v = open(ruta_abs+'\\files\\features_val.p','r') #Llegim els fitxer de la llista per validació
 
-def Evaluate_Ranking(Direct_llista,train_or_val): #Funció declarada passant com a paràmetres 
+def Evaluate_Ranking(Direct_llista_t,Direct_llista_v,train_or_val): #Funció declarada passant com a paràmetres 
 
-    fitxer_anot = open(ruta_abs+"\TerrassaBuildings900\\train\Annotation_"+train_or_val+".txt" , 'r') #Obrim els arxius 
+    fitxer_anot = open(ruta_abs+"\TerrassaBuildings900\\train\Annotation_"+train_or_val+".txt",'r') #Obrim els arxius 
     # d'annotació en funció del valor de 'train_or_val')
     Final_file = open(ruta_abs+"\\files\\Average_precision_"+train_or_val+".txt", 'w') #Obrim l'arxiu on escriurem el AP per cada consulta  
     Final_file2 = open(ruta_abs+"\\files\\Mean_average_precision_"+train_or_val+".txt", 'w') ##Obrim l'arxiu on escriurem el MAP per cada consulta  
@@ -22,22 +23,27 @@ def Evaluate_Ranking(Direct_llista,train_or_val): #Funció declarada passant com
     Final_file2_valid = open(ruta_abs+"\\files\\final_file2_valid.txt" ,'w') #Obrim l'arxiu on escriurem el MAP per cada consulta de validació
     
     
-    for line in Direct_llista:
+    for line in Direct_llista_t:
         Final_file = np.random.rand(1,180) #Obrim el vector aleatori on s'inclouran el total de APs per cada consulta
-        endline = line.index("\n") #Indicació del final de línea de casa vector de AP's
+        final = line.index("\n") #Indicació del final de línea de casa vector de AP's
         if train_or_val == "train":
             fitxer_anot = "annotation_valid.txt"
-            APt = label_ranking_average_precision_score(Direct_llista,fitxer_anot)
+            APt = label_ranking_average_precision_score(Direct_llista_t,fitxer_anot)
             Final_file.append(APt)
             # A continuació escriurem en el fitxer cada línia de les APS per les imatges d'entrenamen
-            Final_file_train.write(line[0:endline] + "For Query "+line+":\t" + str(Final_file).replace("\n","").replace("[[","").replace("]]","") + "\n")
-        else:
+            Final_file_train.write(line[0:final] + "For Query "+line+":\t" + str(Final_file).replace("\n","").replace("[[","").replace("]]","") + "\n")
+    Final_file_train.close() #Tanquem el fitxer corresponent a les imatges d'entrenament
+        
+        
+    for line in Direct_llista_v:
+        Final_file = np.random.rand(1,180) #Obrim el vector aleatori on s'inclouran el total de APs per cada consulta
+        final = line.index("\n") #Indicació del final de línea de casa vector de AP's
+        if train_or_val == "valid":    
             fitxer_anot = "annotation_valid.txt"
-            APv = label_ranking_average_precision_score(Direct_llista,fitxer_anot)
+            APv = label_ranking_average_precision_score(Direct_llista_v,fitxer_anot)
             Final_file.append(APv)
             # A continuació escriurem en el fitxer cada línia de les APS per les imatges de validació
-            Final_file_valid.write(line[0:endline] + "For Query "+line+":\t"+ str(Final_file).replace("\n","").replace("[[","").replace("]]","") + "\n") 
-    Final_file_train.close() #Tanquem el fitxer corresponent a les imatges d'entrenament
+            Final_file_valid.write(line[0:final] + "For Query "+line+":\t"+ str(Final_file).replace("\n","").replace("[[","").replace("]]","") + "\n") 
     Final_file_valid.close() #Tanquem el fitxer corresponent a les imatges de validació
 
 
@@ -50,7 +56,7 @@ def Evaluate_Ranking(Direct_llista,train_or_val): #Funció declarada passant com
         MAP_train = suma_train/sum_elems #Fem la peració per obtenir aquest valor
         Final_file2.append(MAP_train) #Introduïm el valor resultant dintre del array creat
         #A Continuació esciurem el valor resultant del MAP dintre del fitxer de sortida
-        Final_file2_train.write(line[0:endline] + "For Query "+line+":\t" + "Mean_Average_Precision = "+str(Final_file2).replace("\n","").replace("[[","").replace("]]","") + "\n")
+        Final_file2_train.write(line[0:final] + "For Query "+line+":\t" + "Mean_Average_Precision = "+str(Final_file2).replace("\n","").replace("[[","").replace("]]","") + "\n")
     Final_file_train.close() #Tanquem el fitxer per on hem llegit les dades dels APS de cada consulta
     Final_file2_train.close() #Tanquem el ftixer per on hem esccrit els valors del MAP resultants per cada línia
     
@@ -63,7 +69,7 @@ def Evaluate_Ranking(Direct_llista,train_or_val): #Funció declarada passant com
         MAP_valid = suma_valid/sum_elems #Fem la peració per obtenir aquest valor
         Final_file2.append(MAP_valid) #Introduïm el valor resultant dintre del array creat
         #A Continuació esciurem el valor resultant del MAP dintre del fitxer de sortida
-        Final_file2_valid.write(line[0:endline] + "For Query "+line+":\t" + "Mean_Average_Precision = "+str(Final_file2).replace("\n","").replace("[[","").replace("]]","") + "\n")
+        Final_file2_valid.write(line[0:final] + "For Query "+line+":\t" + "Mean_Average_Precision = "+str(Final_file2).replace("\n","").replace("[[","").replace("]]","") + "\n")
     Final_file_valid.close() #Tanquem el fitxer per on hem llegit les dades dels APS de cada consulta
     Final_file2_valid.close() #Tanquem el ftixer per on hem esccrit els valors del lMAP resultants per cada línia
 
