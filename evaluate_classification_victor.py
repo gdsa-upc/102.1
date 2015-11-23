f8eb0d8074deb8a98892a75895026fb87a1cd967
Víctor Casales <victor.casales@estudiant.upc.edu>
2015-11-23 14:55:43 +0100

    Delete evaluate_classification_victor.py

--- evaluate_classification_victor.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import os
-
-ruta = os.path.dirname(os.path.abspath(__file__)) #obtenim la ruta absoluta de la carpeta del projecte
-
-def evaluate_rank(dir_rank,anotations)
-    
-    return ap, man #retornem tant la average precision com la mean average precision
