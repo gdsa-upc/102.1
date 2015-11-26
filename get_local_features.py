302551f159b03ef9601f2a98d42f5de95149e95c
Franc Ortiz <fco.ortiz.castilla@gmail.com>
2015-11-26 18:34:45 +0100

    Update get_local_features.py

--- get_local_features.py
+++ get_local_features.py
@@ -7,13 +7,14 @@ ruta = os.path.dirname(os.path.abspath(__file__)) #obtenim la ruta absoluta de l
 def get_local_features(query):
     img = cv2.imread(ruta+query,1) #obrim la imatge que hi es a la carpeta images
     r = 500.0 / img.shape[1]
-    dim = (500, int(img.shape[0] * r))
-    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
-    sift = cv2.SIFT()
-    kp, des = sift.detectAndCompute(resized,None) #cambiar resized por image cuando queramos hacerl ocon las imagenes completas
+    dim = (500, int(img.shape[0] * r)) # 
+    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA) #redimensiono la imatge per interpolacio fent-la de tamany 500 mes o menys
+    sift = cv2.SIFT() # inicialitzo SIFT 
+    kp, des = sift.detectAndCompute(resized,None) # obtinc els key points amb els seus descriptors
+    #cambiar resized por image cuando queramos hacerl ocon las imagenes completas
     print len(kp) #mostra el nom de punts d'interés que hi han a la imatge
     print (len(des[4])) #mostra num de descriptors del kp 4
-    for k in range(1, len(kp)):
+    for k in range(1, len(kp)): # mostro tots els key points amb els seus descriptors corresponents
         print kp[k] 
         print des[k]
 
