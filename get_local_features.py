dbbf7a82d741bc61bfe75638ff724793eecf8095
victor <victor.casales.hernandez@gmail.com>
2015-11-27 18:26:20 +0100

    entrenament de codebook primera versio

--- get_local_features.py
+++ get_local_features.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 import cv2
 import os
-
+from scipy.cluster.vq import *
 ruta = os.path.dirname(os.path.abspath(__file__)) #obtenim la ruta absoluta de la carpeta del projecte
 
 def get_local_features(query):
     img = cv2.imread(ruta+query,1) #obrim la imatge que hi es a la carpeta images
-    r = 500.0 / img.shape[1]
-    dim = (500, int(img.shape[0] * r)) # 
+    r = 128.0 / img.shape[1]
+    dim = (128, int(img.shape[0] * r)) # Tamaño de 128 orientativo, probar mas adelante con diferentes tamaños
     resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA) #redimensiono la imatge per interpolacio fent-la de tamany 500 mes o menys
     sift = cv2.SIFT() # inicialitzo SIFT 
-    kp, des = sift.detectAndCompute(resized,None) # obtinc els key points amb els seus descriptors
+    kp,des = sift.detectAndCompute(resized,None) # obtinc els key points amb els seus descriptors
     #cambiar resized por image cuando queramos hacerl ocon las imagenes completas
     """ print len(kp) #mostra el nom de punts d'interés que hi han a la imatge
     print (len(des[4])) #mostra num de descriptors del kp 4
     for k in range(1, len(kp)): # mostro tots els key points amb els seus descriptors corresponents
         print kp[k] 
         print des[k]"""
-    return kp,des
-kp,des=get_local_features('/imagen_primerscript/people.jpg') #tendremos que cambiar la foto
-    
+    return whiten(des, check_finite = True)#Si no se deben retornar normalizados cambiamos a return des
+des=get_local_features('/imagen_primerscript/tiger.jpg') #tendremos que cambiar la foto
--- train_kmeans.py
+++ train_kmeans.py
@@ -1,9 +1,12 @@
+# -*- coding: utf-8 -*-
 from get_local_features import get_local_features
-import os
-import cv2
-import numpy as np
-import sklearn.metrics
-from sklearn.metrics import kmeans
+import matplotlib.pyplot as plt
+from scipy.cluster.vq import kmeans, vq
 
-def train_codebook(normalize_descriptors):
+def train_codebook(nclusters,normalized_descriptors):
+    return kmeans(normalized_descriptors,nclusters)#obtenim els clusters de les imatges
     
+des = get_local_features("/imagen_primerscript/tiger.jpg")
+centroide,_ = train_codebook(2,des)
+
+plt.scatter(des[:,0],des[:,1]),plt.scatter(centroide[:,0],centroide[:,1], color ='r'),plt.show()
