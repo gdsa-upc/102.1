163cb13c1772730e467c8e5908e0d07d65b17183
victor <victor.casales.hernandez@gmail.com>
2015-11-28 14:31:34 +0100

    añadido 'main' para que entrenar al sistema con todas las imágenes de entrenamiento, hace falta algun cambio por que se llena la memoria

--- train_kmeans.py
+++ train_kmeans.py
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
 from get_local_features import get_local_features
 import matplotlib.pyplot as plt
-from scipy.cluster.vq import kmeans, vq
+import os
+from scipy.cluster.vq import kmeans,vq
+import numpy as np
 
 def train_codebook(nclusters,normalized_descriptors):
-    return kmeans(normalized_descriptors,nclusters)#obtenim els clusters de les imatges
-    
+    return kmeans(normalized_descriptors,nclusters) #obtenim els centroides de les imatges
+"""    
 des = get_local_features("/imagen_primerscript/tiger.jpg")
 centroide,_ = train_codebook(2,des)
 
 plt.scatter(des[:,0],des[:,1]),plt.scatter(centroide[:,0],centroide[:,1], color ='r'),plt.show()
+"""
+ruta = os.path.dirname(os.path.abspath(__file__)) 
+nfiles = os.listdir(ruta + "/TerrassaBuildings900/train/images")
+descriptors = []
+for file in nfiles:
+    ds = get_local_features("/TerrassaBuildings900/train/images/"+file)
+    descriptors.append(ds)
+centroide,_ = train_codebook(13,descriptors)
+plt.scatter(descriptors[:,0],descriptors[:,1]),plt.scatter(centroide[:,0],centroide[:,1], color='r'), plt.show()
+    
