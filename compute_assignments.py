b96a2dc7689c6b2b57baf17d4d1fafa6d808d05d
gdsa1022 <heimricusdc@gmail.com>
2015-11-29 17:58:07 +0100

    Mejoras en las funciones de ambos codigos
    En principio compila pero me sigue apareciendo el error
    'numpy.core.multiarray failed to import'. Me gustaria que le echarais un
    vistazo

--- compute_assignments.py
+++ compute_assignments.py
@@ -2,34 +2,48 @@
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-from scipy.cluster.vq import vq
+from sklearn.metrics import predict
 from train_kmeans import train_codebook
 from get_local_features import get_local_features
 
-def compute_assignments(codebook,desc,id_image,val_or_train):
+ruta = os.path.dirname(os.path.abspath(__file__)) # Definim la instrucció principal que busca la ruta absoluta del fitxer
+
+def compute_assignments(codebook,desc,id_image,n_cluster):
     #Paràmetres de la funcio: el codebook amb les centroides trobats, els decriptors, les ids de les imatges i si l'indicador de si la carpeta 
-    #on estem es la de validacio o la d'entrenament.     
-    return vq(codebook,desc,id_image,val_or_train) #Retorna els codis assignats des del codebook 
+    #on estem es la de validacio o la d'entrenament.
+    TrainImages = open(ruta+"/TerrassaBuildings900/train/images",'r')
+    ValImages = open(ruta+"/TerrassaBuildings900/val/images",'r')
+    assignments = [] #Declaració del vector d'assignacions que realitzarà
+    for file in TrainImages: #En aquest for omplim la primera part del vector d'assignacions per les imatges d'entrenament 
+        for desc in file:   #en funcio de en quin cluster es trobi el descriptor gràcies a 'predict'. 
+            pred = predict(13,desc)
+            assignments.append(pred)
+    TrainImages.close()
+    for file in ValImages: #En aquest for omplim la primera part del vector d'assignacions per les imatges de validacio
+        for desc in file:  #en funcio de en quin cluster es trobi el descriptor gràcies a 'predict'. 
+            pred = predict(13,desc)
+            assignments.append(pred)
+    ValImages.close()
+    return assignments #Retorna el vector d'assignacions
     
-ruta = os.path.dirname(os.path.abspath(__file__)) 
 nfiles_t = os.listdir(ruta+"/TerrassaBuildings900/train/images")
 nfiles_v = os.listdir(ruta+"/TerrassaBuildings900/val/images")
-descriptors = []
-assignments = []
+descriptors = [] #Declarem el vector de descriptors
+assig = [] #Declarem el vector d'assignacions
 
-for file in nfiles_t:
+for file in nfiles_t: 
    dscrp = get_local_features("/TerrassaBuildings900/train/images"+file)
    descriptors.append(dscrp)
    centroide,_ = train_codebook(13,descriptors)
 assig = compute_assignments(centroide,_,dscrp,file,"train")
-assignments.append(assig)
 
 for file in nfiles_v:
    dscrp = get_local_features("/TerrassaBuildings900/val/images"+file)
    descriptors.append(dscrp)
    centroide,_ = train_codebook(13,descriptors)
 assig = compute_assignments(centroide,_,dscrp,file,"val")
-assignments.append(assig)
 
+#A continuació representarem la grafica amb els descriptors i els centroides mrcats amb color vermell
 plt.scatter(descriptors[:,0],descriptors[:,1]),plt.scatter(centroide[:,0],centroide[:,1],color = 'r'),plt.show()
-print", ".join(assignments)
+#Mes tard, mostrem per pantalla el vector d'assignacions separats amb comes
+print", ".join(assig)
--- construct_bow_vector.py
+++ construct_bow_vector.py
@@ -1,38 +1,56 @@
 #-*- coding: utf-8 -*-    import numpy as np
+import numpy as np
+import pickle
 import os
-import matplotlib.pyplot as plt
-from scipy.cluster.vq import vq
 from sklearn.preprocessing import normalize
 from train_kmeans import train_codebook
 from get_local_features import get_local_features
-from compute_assignments import get_assignments
+from compute_assignments import compute_assignments
 
 def construct_bow_vector(assignments,train_codebook,id_image,val_or_train):
     #Paràmetres de la funcio: els assignments realitzadors sobre els descriptors, el codebook amb les centroides trobats, 
     # els decriptors, les ids de les imatges i si l'indicador de si la carpeta 
-    #on estem es la de validacio o la d'entrenament. 
-    return normalize(assignments,train_codebook,id_image,val_or_train)
-ruta = os.path.dirname(os.path.abspath(__file__)) 
-nfiles_t = os.listdir(ruta+"/TerrassaBuildings900/train/images")
-nfiles_v = os.listdir(ruta+"/TerrassaBuildings900/val/images")
-descriptors = []
-assignments = []
-BoW = []
+    #on estem es la de validacio o la d'entrenament
+    normalized = [] #declaració del vector amb els valors de les assignacions normalitzades entre 0 i 1. 
+    for element in assignments:
+        div = normalize(assignments,train_codebook,id_image,val_or_train)
+        n = assignments[element]/div
+        normalized.append(n)
+    return normalized
+    
+ruta = os.path.dirname(os.path.abspath(__file__)) # Definim la instrucció principal que busca la ruta absoluta del fitxer
+IDs_file_T = open(ruta+"/files/outfile_train.txt", 'r') #obrim l'arxiu que conté les ids de les imatges d'entrenament
+local_features_file_T = open(ruta + "/files/local_features_train.p",'w') #obrim l'arxiu en el que escriurem les caracteristiques
+IDs_file_V = open(ruta+"/files/outfile_val.txt", 'r') #obrim l'arxiu que conté les ids de les imatges de validació
+local_features_file_V = open(ruta + "/files/local_features_val.p",'w') #obrim l'arxiu en el que escriurem les caracteristiques
+feat_vec = dict() #inicialitzem el diccionari buit
+descriptors = [] #Declarem el vector de descriptors
+assignments = [] #Declarem el vector d'assignacions
 
-for file in nfiles_t:
+for line in IDs_file_T:
+    BoW = np.zeros(1,100)#Generem el vector de paraules normalitzades.
+    final = file.index("\n")
     dscrp = get_local_features("/TerrassaBuildings900/train/images"+file)
-    descriptors.append(dscrp)
+    descriptors.append(dscrp) #introduim el vector de descriptors
     centroide,_ = train_codebook(13,descriptors)
-    assig = get_assignments(centroide,_,dscrp,file,"train")
-    assignments.append(assig)
-norm = construct_bow_vector(assig,centroide,_,dscrp,file,"train")
-BoW.append(norm)
+    assig = compute_assignments(centroide,_,dscrp,file,"train") #crida a la funció compute_assignments
+    assignments.append(assig) #Insertem cada assignacio al vector de assignacions
+    norm = construct_bow_vector(assig,centroide,_,dscrp,file,"train") #crida a la funció construct_bow_vector
+    BoW.insert(norm) # insertem cada element normalitzat al vector BoW.
+    feat_vec[line[0:final]] = BoW #Afegim al diccionari el vector de paraules normalitzades.
+    pickle.dump(feat_vec, local_features_file_T) #Escribim el diccionari amb 'pickle'.
+IDs_file_T.close() #Tanquem el directori on es trobaven les imatges d'entrenament.
 
-for file in nfiles_v:
+for line in IDs_file_V:
+    BoW = np.zeros(1,100)#Generem el vector de paraules normalitzades.
+    final = file.index("\n")
     dscrp = get_local_features("/TerrassaBuildings900/val/images"+file)
     descriptors.append(dscrp)
     centroide,_ = train_codebook(13,descriptors)
-    assig = get_assignments(centroide,_,dscrp,file,"val")
-    assignments.append(assig)
-norm = construct_bow_vector(assig,centroide,_,dscrp,file,"val")
-BoW.append(norm)
+    assig = compute_assignments(centroide,_,dscrp,file,"val") #crida a la funció compute_assignments
+    assignments.append(assig) #Insertem cada assignacio al vector de assignacions
+    norm = construct_bow_vector(assig,centroide,_,dscrp,file,"val")
+    BoW.insert(norm) # insertem cada element normalitzat al vector BoW.
+    feat_vec[line[0:final]] = BoW #Afegim al diccionari el vector de paraules normalitzades
+    pickle.dump(feat_vec, IDs_file_V) #Escribim el diccionari amb 'pickle'
+IDs_file_V.close() #Tanquem el directori on es trobaven les imatges de validació.
