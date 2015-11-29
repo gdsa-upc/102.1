b08a0587109f138a2af7d15765522d4ad4cc6523
gdsa1022 <heimricusdc@gmail.com>
2015-11-29 13:27:03 +0100

    Main de BoW incluido

--- construct_bow_vector.py
+++ construct_bow_vector.py
@@ -2,9 +2,37 @@
 import os
 import matplotlib.pyplot as plt
 from scipy.cluster.vq import vq
+from sklearn.preprocessing import normalize
 from train_kmeans import train_codebook
 from get_local_features import get_local_features
 from compute_assignments import get_assignments
 
-def build_bow(train_codebook,id_image):
-    
+def build_bow(assignments,train_codebook,id_image,val_or_train):
+    #Paràmetres de la funcio: els assignments realitzadors sobre els descriptors, el codebook amb les centroides trobats, 
+    # els decriptors, les ids de les imatges i si l'indicador de si la carpeta 
+    #on estem es la de validacio o la d'entrenament. 
+    return normalize(assignments,train_codebook,id_image,val_or_train)
+ruta = os.path.dirname(os.path.abspath(__file__)) 
+nfiles_t = os.listdir(ruta+"/TerrassaBuildings900/train/images")
+nfiles_v = os.listdir(ruta+"/TerrassaBuildings900/val/images")
+descriptors = []
+assignments = []
+BoW = []
+
+for file in nfiles_t:
+    dscrp = get_local_features("/TerrassaBuildings900/train/images"+file)
+    descriptors.append(dscrp)
+    centroide,_ = train_codebook(13,descriptors)
+    assig = get_assignments(centroide,_,dscrp,file,"train")
+    assignments.append(assig)
+norm = normalize(assig,centroide,_,dscrp,file,"train")
+BoW.append(norm)
+
+for file in nfiles_v:
+    dscrp = get_local_features("/TerrassaBuildings900/val/images"+file)
+    descriptors.append(dscrp)
+    centroide,_ = train_codebook(13,descriptors)
+    assig = get_assignments(centroide,_,dscrp,file,"val")
+    assignments.append(assig)
+norm = normalize(assig,centroide,_,dscrp,file,"val")
+BoW.append(norm)
