92c0e2ccd95ecba4faf29befcc5bb6a9db36270f
victor <victor.casales.hernandez@gmail.com>
2015-11-29 20:01:18 +0100

    segunda version kmeans

--- train_kmeans.py
+++ train_kmeans.py
@@ -7,18 +7,18 @@ import numpy as np
 
 def train_codebook(nclusters,normalized_descriptors):
     return kmeans(normalized_descriptors,nclusters) #obtenim els centroides de les imatges
-"""    
+"""  
 des = get_local_features("/imagen_primerscript/tiger.jpg")
 centroide,_ = train_codebook(2,des)
 
 plt.scatter(des[:,0],des[:,1]),plt.scatter(centroide[:,0],centroide[:,1], color ='r'),plt.show()
 """
 ruta = os.path.dirname(os.path.abspath(__file__)) 
-nfiles = os.listdir(ruta + "/TerrassaBuildings900/train/images")
+nfiles = os.listdir(ruta + "/imagen_primerscript")
 descriptors = []
 for file in nfiles:
-    ds = get_local_features("/TerrassaBuildings900/train/images/"+file)
-    descriptors.append(ds)
-centroide,_ = train_codebook(13,descriptors)
-plt.scatter(descriptors[:,0],descriptors[:,1]),plt.scatter(centroide[:,0],centroide[:,1], color='r'), plt.show()
-    
+    ds = get_local_features("/imagen_primerscript/"+file)
+    for feat in ds:
+        descriptors.append(feat)
+centroides,_ = train_codebook(1,descriptors)
+plt.plot(descriptors[:,0],descriptors[:,2]), plt.show()
