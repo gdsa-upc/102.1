2b0129d0235784ea687168824b12cac9e69da3c4
gdsa1022 <heimricusdc@gmail.com>
2015-11-29 19:31:54 +0100

    Retoques de la función compute_assignments

--- compute_assignments.py
+++ compute_assignments.py
@@ -2,28 +2,16 @@
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-from sklearn.metrics import predict
+from scipy.cluster.vq import vq,whiten
 from train_kmeans import train_codebook
 from get_local_features import get_local_features
 
 ruta = os.path.dirname(os.path.abspath(__file__)) # Definim la instrucció principal que busca la ruta absoluta del fitxer
 
-def compute_assignments(codebook,desc,id_image,n_cluster):
-    #Paràmetres de la funcio: el codebook amb les centroides trobats, els decriptors, les ids de les imatges i si l'indicador de si la carpeta 
-    #on estem es la de validacio o la d'entrenament.
-    TrainImages = open(ruta+"/TerrassaBuildings900/train/images",'r')
-    ValImages = open(ruta+"/TerrassaBuildings900/val/images",'r')
-    assignments = [] #Declaració del vector d'assignacions que realitzarà
-    for file in TrainImages: #En aquest for omplim la primera part del vector d'assignacions per les imatges d'entrenament 
-        for desc in file:   #en funcio de en quin cluster es trobi el descriptor gràcies a 'predict'. 
-            pred = predict(13,desc)
-            assignments.append(pred)
-    TrainImages.close()
-    for file in ValImages: #En aquest for omplim la primera part del vector d'assignacions per les imatges de validacio
-        for desc in file:  #en funcio de en quin cluster es trobi el descriptor gràcies a 'predict'. 
-            pred = predict(13,desc)
-            assignments.append(pred)
-    ValImages.close()
+def compute_assignments(codebook,desc):
+    #Paràmetres de la funcio: el codebook amb les centroides trobats i els decriptors
+    norm_desc = whiten(desc) # Normaliza descriptores
+    assignments,_ = vq(norm_desc, codebook) # la funció vq elabora el vector d'assignacions
     return assignments #Retorna el vector d'assignacions
     
 nfiles_t = os.listdir(ruta+"/TerrassaBuildings900/train/images")
