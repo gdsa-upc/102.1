3cbd4c0cf97738f9efd58596b11146debe54f7eb
victor <victor.casales.hernandez@gmail.com>
2015-11-30 18:36:12 +0100

    creacion del rootsift, extracción de caracteristicas con rootsift y cálculo de kmeans

---
+++ src/functions/train_codebook.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from scipy.cluster.vq import kmeans
+from get_local_features import get_local_features
+import matplotlib.pyplot as plt
+
+def train_codebook(nclusters,normalized_descriptors):
+    return kmeans(normalized_descriptors,nclusters) #obtenim els centroides de les imatges
+
+if __name__ == "__main__":
+    desc = get_local_features("../imagen_primerscript/people.jpg") #obtenim els descriptors de la imatge
+    centroides,_ = train_codebook(2,desc) #ccalculem els centroides de la imatge
+    plt.scatter(desc[:,0],desc[:,1]),plt.scatter(centroides[:,0],centroides[:,1],color = 'r'),plt.show() #mostrem els centroides i els descriptors
