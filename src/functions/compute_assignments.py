97df8bb36e5913b062a4b87645775cab5bfeeaf1
victor <victor.casales.hernandez@gmail.com>
2015-11-30 22:54:02 +0100

    retorno del assignments normalizados.

--- src/functions/compute_assignments.py
+++ src/functions/compute_assignments.py
@@ -10,16 +10,16 @@ ruta = os.path.dirname(os.path.abspath(__file__)) # Definim la instrucció princ
 
 def compute_assignments(codebook,desc):
     #Paràmetres de la funcio: el codebook amb les centroides trobats i els decriptors
-    return vq(whiten(desc), codebook)
+    return whiten(vq(whiten(desc), codebook)) #tornem les distancies normalitzades
 
 if __name__ == "__main__":
     dsc = []
     desc = get_local_features("../imagen_primerscript/people.jpg")
-    for feat in desc:
-        dsc.append(desc)
     desc2 = get_local_features("../imagen_primerscript/tiger.jpg")
+    for feat in desc:
+        dsc.append(feat)
     for feat in desc2:
-        dsc.append(desc2)
-    print len(dsc)
+        dsc.append(feat)
     codebook,_ = train_codebook(5,dsc)
-    #assig = compute_assignments(codebook,dsc)    
+    assig = compute_assignments(codebook,dsc)
+    #plt.scatter(codebook[:,0],codebook[:,1]),plt.scatter(dsc[:,0],dsc[:,1], color='r'), plt.show() 
