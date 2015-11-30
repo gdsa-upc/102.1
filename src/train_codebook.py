e599bfbb69322a2f0799d4c66b6b22db01d4c5ca
monicachs <monica_bdn_92@hotmail.com>
2015-11-30 12:24:22 +0200

    creacio main

--- src/train_codebook.py
+++ src/train_codebook.py
@@ -7,14 +7,13 @@ import numpy as np
 
 def train_codebook(nclusters,normalized_descriptors):
     return kmeans(normalized_descriptors,nclusters) #obtenim els centroides de les imatges
-'''
-ruta = os.path.dirname(os.path.abspath(__file__)) #obtenim la ruta del l'arxiu
-nfiles = os.listdir(ruta + "/TerrassaBuildings900/train/images") #llistem tots els arxius de la carpeta 
-descriptors = [] #inicialitzem el vector descriptors
-for file in nfiles:
-    ds = get_local_features("/TerrassaBuildings900/train/images/"+file) #obtenim els descriptors de cada imatge
-    for feat in ds:
-        descriptors.append(feat) #guardem tots els descriptors de totes les imatges
-centroides,_ = train_codebook(12,descriptors) #calculem els centroides de les imatges
-#plt.scatter(centroides[:,0],centroides[:,1]), plt.show()
-'''
+
+if __name__ == "__main__"":
+    ruta = os.path.dirname(os.path.abspath(__file__)) #obtenim la ruta del l'arxiu
+    nfiles = os.listdir(ruta + "/TerrassaBuildings900/train/images") #llistem tots els arxius de la carpeta 
+    descriptors = [] #inicialitzem el vector descriptors
+    for file in nfiles:
+        ds = get_local_features("/TerrassaBuildings900/train/images/"+file) #obtenim els descriptors de cada imatge
+        for feat in ds:
+            descriptors.append(feat) #guardem tots els descriptors de totes les imatges
+    centroides,_ = train_codebook(100,descriptors) #calculem els centroides de les imatges
