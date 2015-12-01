6f16ec1640b215dfdd1b44af33bc21df8961f1de
victor <victor.casales.hernandez@gmail.com>
2015-12-01 16:19:07 +0100

    .gitignore is now working

--- src/functions/__init__.py
+++
@@ -1,6 +0,0 @@
-from get_local_features import get_local_features
-from compute_assignments import compute_assignments
-from rootsift import RootSIFT
-from train_codebook import train_codebook
-
-__all__ = ["get_local_features","compute_assignments","RootSIFT","train_codebook"]
