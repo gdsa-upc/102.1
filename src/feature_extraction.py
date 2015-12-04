b20d6e3c46ccc806433927ab64154db3fc145f63
victor <victor.casales.hernandez@gmail.com>
2015-12-04 18:41:10 +0100

    realizacion, todavía no definitiva de los cambios en el "main" y en algunos archivos

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -4,32 +4,31 @@ import pickle
 
 def get_descriptors(path):
     nfiles = os.listdir(path)
-    dsc = []
+    dsc_all = []
+    dsc_ind = {}
     for file in nfiles:
-        desc = get_local_features(path + "/" + file)
-        for features in desc:
-            dsc.append(features)
-    return dsc
+        filename = file[0:file.index(".")]
+        dsc_ind[filename] = get_local_features(path + "/" + file)
+        for feat in dsc_ind[filename]:
+            dsc_all.append(feat)
+    return dsc_all,dsc_ind
 
-def assignments_train(path):
-    dsc = get_descriptors(path)
-    centroides,_ = train_codebook(100,dsc)
-    clase = compute_assignments(centroides,dsc)
-    hist = construct_bow_vector(clase)
-    return centroides, hist
+def train(dsc,nclusters):
+    centroides,_ = train_codebook(nclusters,dsc)
+    return centroides
 
-def assignments_val(path,centroides):
-     dsc = get_descriptors(path)
-     clase = compute_assignments(centroides,dsc)
-     hist = construct_bow_vector(clase)
-     return hist
+def save_bow(centroides,dsc,val_or_train):
+    bow = dict()
+    for l in dsc:
+        assig,k = compute_assignments(centroides,dsc[l])
+        bow[l] = construct_bow_vector(assig,k)
+    bow_file = open("../files/bow_" + val_or_train + ".p",'w')
+    pickle.dump(bow,bow_file)
+    bow_file.close()
 
 if __name__ == "__main__":
-    centroides, hist_train = assignments_train("../TerrassaBuildings900/train/images")
-    BoW_train_file = open("../files/features_train.p", 'w')
-    pickle.dump(hist_train, BoW_train_file)
-    BoW_train_file.close()
-    hist_val = assignments_val("../TerrassaBuildings900/val/images",centroides)
-    BoW_val_file = open("../files/features_val.p", 'w')
-    pickle.dump(hist_val, BoW_val_file)
-    BoW_val_file.close()
+    dsc_all_train, dsc_ind_train = get_descriptors("../TerrassaBuildings900/train/images")
+    centroides = train(dsc_all_train,50)
+    save_bow(centroides,dsc_ind_train,"train")
+    _, dsc_ind_val = get_descriptors("../TerrassaBuildings900/val/images")
+    save_bow(centroides,dsc_ind_val,"val")   
