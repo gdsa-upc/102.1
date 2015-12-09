f9887b00e8c5254c53d2855fbe8391bb9754d8fc
monicachs <monica_bdn_92@hotmail.com>
2015-12-09 13:49:50 +0200

    rank
    millores

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -27,8 +27,8 @@ def save_bow(centroides,dsc,val_or_train,nclusters):
     bow_file.close()
 
 if __name__ == "__main__":
-    """dsc_all_train, dsc_ind_train = get_descriptors("../TerrassaBuildings900/train/images")
-    nclusters = 50
+    dsc_all_train, dsc_ind_train = get_descriptors("../TerrassaBuildings900/train/images")
+    nclusters = 100
     centroides = train(dsc_all_train,nclusters)
     save_bow(centroides,dsc_ind_train,"train",nclusters)
     _, dsc_ind_val = get_descriptors("../TerrassaBuildings900/val/images")
@@ -42,3 +42,4 @@ if __name__ == "__main__":
     for i in tt:
         if i not in ln:
             ln.append(i)
+    """
