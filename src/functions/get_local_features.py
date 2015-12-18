de4b1ca05889256d2a97f8ae16eeef3d5fe86a66
victor <victor.casales.hernandez@gmail.com>
2015-12-18 18:02:22 +0100

    redimensionado a 300 y rootsift

--- src/functions/get_local_features.py
+++ src/functions/get_local_features.py
@@ -8,13 +8,13 @@ def get_local_features(query):
     r = 300.0 / image.shape[1]
     dim = (300, int(image.shape[0] * r)) # Tamaño de 128 orientativo, probar mas adelante con diferentes tamaños
     image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA) #redimensiono la imatge per interpolacio fent-la de tamany 500 mes o menys    
-    """# detect Difference of Gaussian keypoints in the image
+    # detect Difference of Gaussian keypoints in the image
     detector = cv2.FeatureDetector_create("SIFT")
     kps = detector.detect(image)
     # extract RootSIFT descriptors
     rs = RootSIFT()
-    (kps, descs) = rs.compute(image, kps)"""
-    kp,descs = cv2.SIFT().detectAndCompute(image,None)
+    (kps, descs) = rs.compute(image, kps)
+    #kp,descs = cv2.SIFT().detectAndCompute(image,None)
     return descs #return the descriptors
 if __name__ == "__main__":
     a = get_local_features("../imagen_primerscript/people.jpg")
